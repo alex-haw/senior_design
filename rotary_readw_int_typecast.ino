f61a2e52b7e3b555ea9b200808993de51fc9b73e
Alex <64337159+alex-haw@users.noreply.github.com>
2021-02-02 16:16:07 -0600

    Add files via upload
    Evan is morbidly obese and a Grinch

---
+++ rotary_readw_int_typecast.ino
@@ -0,0 +1,61 @@
+int pin1 = 2;
+int pin2 = 3;
+int on_off = 4;
+int units = 5;
+int hld_clr = 6;
+int no1_no1 = 7;
+
+int Pos = 0; 
+int State;
+int LastState;  
+
+const float pi = 3.14159;
+
+const float R = 7.1882;
+const int N = 24;
+
+String nigger = "";
+
+float distance = 0;
+int seq = 0;
+void setup() { 
+  pinMode (pin1 ,INPUT);
+  pinMode (pin2 ,INPUT);
+  pinMode (on_off, OUTPUT);
+  pinMode (units, OUTPUT);
+  pinMode (hld_clr, OUTPUT);
+  pinMode (no1_no1, OUTPUT);
+  
+ // attachInterrupt(1, rotary, CHANGE);
+  Serial.begin(9600); // open the serial port at 9600 bps:
+  
+  LastState = digitalRead(pin1);    
+
+} 
+
+void loop(){
+  distance = ((2*pi*R)/N) * Pos ;  
+  Serial.println(-1*distance);
+  delay(10);  
+}
+
+void loop() { 
+  distance = ((2*pi*R)/N) * Pos ;  
+  Serial.println(int(-1*distance));
+  delay(10);
+}
+
+ 
+void rotary(){
+  State = digitalRead(pin1);
+   if (State != LastState){     
+     if (digitalRead(pin2) != State) { 
+       Pos ++;
+     } 
+     
+     else {
+       Pos --;
+     }
+   } 
+   
+ }

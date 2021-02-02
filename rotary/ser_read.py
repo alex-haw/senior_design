4f83f28e99a15d15553529d132c9b3c22d91f246
Evan <elbrowning17@gmail.com>
2021-02-02 10:54:30 -0600

    added log to ser_read.py

--- rotary/ser_read.py
+++ rotary/ser_read.py
@@ -1,1 +1,21 @@
-# later
+# https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/#Serial_via_USB
+# This is an experimental program to read serial data from an arduino through usb
+import serial, time
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = 0.1) # initialize serial arduino object
+arduino.flush() # Clear input and output buffer
+
+# Enable temp readings
+from gpiozero import CPUTemperature
+
+f = open('log.csv', 'a')
+
+while True:
+	#data = arduino.readline().decode('ascii').rstrip() # decode serial bytes and remove trailing characters (\n)
+	#if data:
+	#print(data)
+	# LOG DATA
+#	cpu = CPUTemperature()
+#	temp = str(cpu.temperature)
+#	date = strftime("%Y-%m-%d %H:%M:S")
+#	log.write("{0},{1}/n".format(date,temp))
+	time.sleep(1)

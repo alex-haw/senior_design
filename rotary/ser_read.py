ed5eb9d6e6d8821baa8070b13bb896ceedafe8c6
Evan <elbrowning17@gmail.com>
2021-02-02 11:29:08 -0600

    log test

--- rotary/ser_read.py
+++ rotary/ser_read.py
@@ -1,5 +1,6 @@
 # https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/#Serial_via_USB
 # This is an experimental program to read serial data from an arduino through usb
+from time import sleep, strftime, time
 import serial, time
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = 0.1) # initialize serial arduino object
 arduino.flush() # Clear input and output buffer
@@ -7,15 +8,18 @@ arduino.flush() # Clear input and output buffer
 # Enable temp readings
 from gpiozero import CPUTemperature
 
-f = open('log.csv', 'a')
+log = open('log.csv', 'a')
 
 while True:
-	#data = arduino.readline().decode('ascii').rstrip() # decode serial bytes and remove trailing characters (\n)
-	#if data:
-	#print(data)
-	# LOG DATA
-#	cpu = CPUTemperature()
-#	temp = str(cpu.temperature)
-#	date = strftime("%Y-%m-%d %H:%M:S")
-#	log.write("{0},{1}/n".format(date,temp))
+	data = arduino.readline().decode('ascii').rstrip() # decode serial bytes and remove trailing characters (\n)
+	if data:
+		print(data)
+		#log data
+		cpu = CPUTemperature()
+		temp = str(cpu.temperature)
+		message = str("Trial on: ")
+		dateYMD = strftime("%Y-%m-%d")
+		timeHMS = strftime("%H:%M:%S")
+		print("Writing to log.csv\n")
+		log.write("{0},{1},{2},{3},{4}\n".format(message,dateYMD,timeHMS,str(data),temp))
 	time.sleep(1)

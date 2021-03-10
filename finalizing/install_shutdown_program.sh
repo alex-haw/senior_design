e7f45f32f9a0da13137084d17ab2289fe2b2efa8
Evan Browning <elbrowning17@gmail.com>
2021-03-09 19:31:28 -0600

    some stuff and a thang

--- finalizing/install_shutdown_program.sh
+++ finalizing/install_shutdown_program.sh
@@ -1,8 +1,8 @@
-sudo mv listen-for-shutdown.py /usr/local/bin/
-sudo chmod +x /usr/local/bin/listen-for-shutdown.py
-sudo mv listen-for-shutdown.sh /etc/init.d/
-sudo chmod +x /etc/init.d/listen-for-shutdown.sh
-sudo update-rc.d listen-for-shutdown.sh defaults
-sudo /etc/init.d/listen-for-shutdown.sh start
-echo "successfuly installed"
-
+while true; do
+    read -p "This script will add sleep/wake external button capability. You must run this as root. Continue?" yn
+    case $yn in
+        [Yy]* ) sudo cp listen-for-shutdown.py /usr/local/bin/; sudo chmod +x /usr/local/bin/listen-for-shutdown.py; sudo cp listen-for-shutdown.sh /etc/init.d/; sudo chmod +x /etc/init.d/listen-for-shutdown.sh; sudo update-rc.d listen-for-shutdown.sh defaults; sudo /etc/init.d/listen-for-shutdown.sh start; echo "successfuly installed"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
